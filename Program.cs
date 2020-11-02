80e2932be336fe215d4417b89e7ced65a8258a2a
Antonbrn <anton.brannstrom@cmeducations.com>
2020-11-02 17:21:03 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new double[3] { 10.2, 24.3, 53.5 };
+
+            var result = 0.0;
+
+            foreach(double num in numbers)
+            {
+                result = result + num + 1;
+            }
+
+            Console.WriteLine(result);
+
+            if(args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            } else
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
