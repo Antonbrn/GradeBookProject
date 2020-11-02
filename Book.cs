edcf26a627d9bbcd1e2ca4d3ce867aeac38a6b66
Anton Brännström <antonbrannstrom@hotmail.com>
2020-11-02 19:48:18 +0100

    Added Book.cs with Addgrade and showgrades methods

---
+++ Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    class Book
+    {
+        //Constructor
+        public Book(string name){
+            Name = name;
+            grades = new List<double>();
+        }
+
+
+        //Methods
+        public void AddGrade(double grade)
+        {
+            if(grade >= 0 && grade <= 100)
+            {
+                grades.Add(grade);
+            }
+        }
+        
+        public void ShowGrades()
+        {
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            var averageGrade = 0.0;
+            foreach(var grade in grades)
+            {
+                highGrade = Math.Max(grade, highGrade);
+                lowGrade = Math.Min(grade, lowGrade);
+                averageGrade += grade / grades.Count;
+            }
+
+            Console.WriteLine($"Highest grade:{highGrade}");
+            Console.WriteLine($"Lowest grade:{lowGrade}");
+            Console.WriteLine($"Average grade:{averageGrade}");
+        }
+
+        //Field
+        private List<double> grades;
+        private string Name;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var numbers = new double[3] { 10.2, 24.3, 53.5 };
+            var book = new Book("Anton's Grade Book");
 
-            var result = 0.0;
-
-            foreach(double num in numbers)
-            {
-                result = result + num + 1;
-            }
-
-            Console.WriteLine(result);
-
-            if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello {args[0]}!");
-            } else
-            {
-                Console.WriteLine("Hello!");
-            }
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowGrades();
+         
         }
     }
 }
