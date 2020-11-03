5d534911eb4e580ebc7d5242b3849208cfa6a2e0
Antonbrn <anton.brannstrom@cmeducations.com>
2020-11-03 01:58:23 +0100

    Deleted test file, got errors. Added Statistics.cs and retructured program.cs

---
+++ GradeBook/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    class Book
+    {
+        //Constructor
+        public Book(string name){
+            Name = name;
+            grades = new List<double>();
+        }
+
+
+        //Methods
+        public void AddGrade(double grade)
+        {
+            if(grade >= 0 && grade <= 100)
+            {
+                grades.Add(grade);
+            }
+        }
+        
+        public Statistics GetGrades()
+        {
+            var result = new Statistics();
+            result.Average = 0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                result.High = Math.Max(grade, result.High);
+                result.Low = Math.Min(grade, result.Low);
+                result.Average += grade;
+            }
+            result.Average /= grades.Count;
+
+            return result;
+        }
+
+        //Field
+        private List<double> grades;
+        private string Name;
+    }
+}
---
+++ GradeBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main()
+        {
+            var book = new Book("Anton's Grade Book");
+
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+
+            var result = book.GetGrades();
+
+            Console.WriteLine($"The average grade is: {result.Average}");
+            Console.WriteLine($"The highest grade is: {result.High}");
+            Console.WriteLine($"The lowest grade is: {result.Low}");
+
+        }
+    }
+}
