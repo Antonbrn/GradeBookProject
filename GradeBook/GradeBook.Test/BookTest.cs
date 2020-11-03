6d6753b6e83f5c470301c1c160aab52a829fea6a
Antonbrn <anton.brannstrom@cmeducations.com>
2020-11-03 02:52:46 +0100

    Added a test project

---
+++ GradeBook/GradeBook.Test/BookTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using GradeBook;
+
+namespace GradeBook.Test
+{
+    public class BookTest
+    {
+        [Fact]
+        public void test1()
+        {
+            Book book = new Book("");
+            
+        }
+    }
+}
